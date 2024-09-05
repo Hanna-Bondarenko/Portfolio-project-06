eea34ef91285e77e5438e762efb6b8bf8a309ff8
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-09-05 21:12:17 +0300

    img

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+import axios from 'axios';
+
+import './js/about-me';
+import './js/projects';
+import './js/faq';
+import './js/covers';
+import './js/reviews';
+import './js/menu';
+import './js/work-together';
